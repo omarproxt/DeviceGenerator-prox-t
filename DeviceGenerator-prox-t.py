d5a4d04ded202281a86dcf7465f34cf7f7ff5984
omarproxt <102703203+omarproxt@users.noreply.github.com>
2023-01-13 03:35:53 +0200

    Update DeviceGenerator-prox-t.py

--- DeviceGenerator-prox-t.py
+++ DeviceGenerator-prox-t.py
@@ -1,3 +1,18 @@
+import requests, pyfiglet,time,sys,webbrowser 
+Ab='\033[1;92m'
+aB='\033[1;91m'
+AB='\033[1;96m'
+aBbs='\033[1;93m'
+AbBs='\033[1;95m'
+A_bSa = '\033[1;31m'
+a_bSa = '\033[1;32m'
+faB_s = '\033[2;32m'
+a_aB_s = '\033[2;39m'
+Ba_bS = '\033[2;36m'
+Ya_Bs = '\033[1;34m'
+S_aBs = '\033[1;33m'
+logo = pyfiglet.figlet_format('omarproxt')
+print(a_bSa+logo)
 from hmac import new
 from os import urandom
 from hashlib import sha1
@@ -17,3 +32,5 @@ def generation_process(count: int):
     	print(f"Generated {count} deviceIDs")
     
 generation_process(count=int(input("How much deviceID generate••>>")))
+
+print('\033[1;33mThe device is stored at omarproxt\033[1;91m')
